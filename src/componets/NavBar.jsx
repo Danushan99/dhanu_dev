8317414dc393a03b1d24a1df839369451bbd9aa1
danuthasan08 <it20051648@my.sliit.lk>
2024-04-05 12:46:23 +0530

    inst alogo

--- src/componets/NavBar.jsx
+++ src/componets/NavBar.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 // react icons
-import { FaXmark, FaBars } from "react-icons/fa6";
+import { FaXmark, FaBars, FaInstagram, FaFacebookF } from "react-icons/fa6";
 
 const NavBar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -32,19 +32,21 @@ const NavBar = () => {
   ];
 
   return (
-
     <div
-      className={`w-full ${isSticky ? "bg-home" : "bg-home"
-        } fixed  left-0 right-0`}
+      className={`w-full ${
+        isSticky ? "bg-home" : "bg-home"
+      } fixed  left-0 right-0`}
     >
       <nav className='py-4 lg:px-14 px-4 ${isSticky ? "sticky top-0 right-0 left-0 border-b bg-gray-4duration-300 : "" }'>
         <div className="flex justify-between items-center text_base gap-8  border-b-2 border-white ">
-        {/* border-b-2 border-black  */}
+          {/* border-b-2 border-black  */}
           <a
             href="/"
             className="text-2xl font-semibold flex items-center space-x-3"
           >
-            <span className="text-3xl font-Butler-regular ">Danushan Ravendran</span>
+            <span className="text-3xl font-Butler-regular ">
+              Danushan Ravendran
+            </span>
           </a>
           <ul className="md:flex space-x-12 hidden">
             {navItems.map(({ link, path }) => (
@@ -58,18 +60,37 @@ const NavBar = () => {
           </ul>
 
           <div className="space-x-12 hidden lg:flex items-center">
-            <a
+            {/* <a
               href="/"
               className="hidden lg:flex items-center text-black hover: text-primary-ash"
             >
               {" "}
               Login
+            </a> */}
+            <a
+              href="https://www.instagram.com/dhanu_thazan_/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <button className="text-black bg-transparent hover:text-gray-100 font-bold text-xl outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150">
+                <FaInstagram className="w-7 h-7" />
+              </button>
             </a>
-            <button className=" bg-gray-1 text-white py-2 px-4 transition-all duration-300 rounded hover:bg--gray-1">
+            <a
+              href="https://www.facebook.com/danu.deviil/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <button className="text-black  hover:text-gray-100 font-bold text-xl outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150">
+                <FaFacebookF className="w-7 h-7" />
+              </button>
+            </a>
+
+            {/* <button className=" bg-gray-1 text-white py-2 px-4 transition-all duration-300 rounded hover:bg--gray-1">
               Sign up
-            </button>
+            </button> */}
           </div>
-          
+
           {/* menu for btn mobile */}
           <div className="md:hidden">
             <button
@@ -86,16 +107,17 @@ const NavBar = () => {
         </div>
         {/* nav for mobile */}
         <div
-          className={
-            `space-y-4 px-4 mt-16 py-7 bg-white ${ isMenuOpen ? "block fixed top-0 right-0 left-0" : "hidden" }`
-          } >
+          className={`space-y-4 px-4 mt-16 py-7 bg-white ${
+            isMenuOpen ? "block fixed top-0 right-0 left-0" : "hidden"
+          }`}
+        >
           {navItems.map(({ link, path }) => (
             <li
               key={path}
               className="block text-base text-black hover:text-yello first:font-medium"
             >
               <a href={`/${path}`}>{link}</a>
-            </li> 
+            </li>
           ))}
         </div>
       </nav>
