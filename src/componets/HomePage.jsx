41df3ee4bbfd2e74414fa89ef8cc949a1e19f963
danuthasan08 <it20051648@my.sliit.lk>
2024-04-07 21:56:26 +0530

    fix issus css

--- src/componets/HomePage.jsx
+++ src/componets/HomePage.jsx
@@ -1,16 +1,17 @@
 import React from "react";
 import NavBar from "./NavBar";
 import AboutPage from "./About";
-import SliderView from "./SliderView";
-import Card from "./Card";
+
+
 import Top from "./Projects";
 import Footer from "./Footer";
+import Portfolio from "./Portfolio";
 
 const HomePage = () => {
   return (
     <div className="bg-Home">
       <NavBar />
-      <SliderView />
+      <Portfolio />
       <AboutPage />
       <Top/>
       <Footer/>
--- src/componets/NavBar.jsx
+++ src/componets/NavBar.jsx
@@ -38,11 +38,16 @@ const NavBar = () => {
         isSticky ? "bg-home" : "bg-home"
       } fixed  left-0 right-0`}
     >
-  
-      <div className={`flex justify-between py-2 px-20 bg-cont font-Jost ${isSticky ? "hidden" : "block"}`}>
-  <h1>SOFTWARE ENGINEER</h1>
-  <h1>TEXT OR TALK • <a href="tel:+94779299587">+94 779299587</a></h1>
-</div>
+      <div
+        className={`flex justify-between py-2 px-20 bg-cont font-Jost ${
+          isSticky ? "hidden" : "block"
+        }`}
+      >
+        <h1>SOFTWARE ENGINEER</h1>
+        <h1>
+          TEXT OR TALK • <a href="tel:+94779299587">+94 779299587</a>
+        </h1>
+      </div>
       <nav className='py-4 bg-nav-bar lg:px-14 px-4 ${isSticky ? "sticky top-0 right-0 left-0 border-b transition-all bg-gray-4 duration-300 : "" }'>
         <div className="flex justify-between items-center text_base gap-8  border-b-2 border-white ">
           {/* border-b-2 border-black  */}
