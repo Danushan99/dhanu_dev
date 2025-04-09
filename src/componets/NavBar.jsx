aeefaae0f1f328e505d205710ecd737964ede5bb
Danushan99 <danuthasan08@gmail.com>
2025-04-09 21:14:21 +0530

    refactor: remove unused import comment in NavBar component

--- src/componets/NavBar.jsx
+++ src/componets/NavBar.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-// react icons
 import { FaXmark, FaBars, FaInstagram, FaFacebookF } from "react-icons/fa6";
 import { Link } from "react-router-dom";
 
